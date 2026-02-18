7fa620b57867025fd2d0ae71f41ba53ab0e1f214
arifulla-abbas <arifulla.abbas.ghs@gmail.com>
2026-02-18 16:57:37 +0530

    playwright-ts next commit

--- tests/TC-02_pim-module.spec.ts
+++ tests/TC-02_pim-module.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from '../fixtures/hooks-fixture';
 import pimModuleData from '../test-data/pim-module-data.json';
 
-test('[PIM]Verify that a new employee is successfully created under the PIM module', { tag: ['@UI', '@UAT'], annotation: { type: 'Test Case Link', description: 'JIRA link: ORANGEHRM-1004' } }, async ({ gotoOrangeHRM, leftHandNavigationPage, pimPage }) => {
+test('[PIM]Verify that a new employee is successfully created under the PIM module',
+    { tag: ['@UI', '@UAT'], annotation: { type: 'Test Case Link', description: 'JIRA link: ORANGEHRM-1004' } }, async ({ gotoOrangeHRM, leftHandNavigationPage, pimPage }) => {
     await test.step('Open PIM module', async () => {
         await leftHandNavigationPage.clickPimLink();
     });
